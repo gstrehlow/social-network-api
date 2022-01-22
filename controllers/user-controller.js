7d0532ea2179ddc8586162464e126cb125e8f0fc
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-22 16:50:06 -0600

    implemented user.find

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,14 @@
-// code 
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+};
+
+module.exports = userController;
