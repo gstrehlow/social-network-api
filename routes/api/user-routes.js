fc8b5f10acb5bf7f6afcb8dd29491da2e1a0128b
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-22 17:15:54 -0600

    added the find one get method

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 const {
     getAllUsers,
     createUser,
+    getUserById,
+    updateUser,
+    deleteUser
 } = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
