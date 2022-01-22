025a911ff2fc70043ffce0ede7da18c658fc4de6
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-22 17:47:29 -0600

    fixed the schema errors with 'toJSON'

--- models/Thought.js
+++ models/Thought.js
@@ -39,12 +39,15 @@ const ThoughtSchema = new Schema({
         required: true
     },
     reactions: [ReactionSchema],
+},
+{
     toJSON: {
         virtuals: true,
         getters: true
     },
     id: false
-});
+}
+);
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
--- models/User.js
+++ models/User.js
@@ -25,10 +25,13 @@ const UserSchema = new Schema({
             ref: "User"
         }
     ],
+},
+{
     toJSON: {
         virtuals: true
     }
-});
+}
+);
 
 UserSchema.virtual("friendCount").get(function () {
     return this.friends.length;
