3afec3cd898b5e5776f1c0ef81205bc886206d51
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-22 14:49:02 -0600

    setup the user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,32 @@
-// code 
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /.+\@.+\..+/
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+});
+
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+})
