88d38131fa90ad11ef1381da0a23f3e7e4b6c5b8
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-23 21:05:10 -0600

    populated the external documents

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,9 @@ const { User } = require('../models');
 const userController = {
     getAllUsers(req, res) {
         User.find({})
+        .populate({ path: 'thoughts', select: '-__v' })
+        .populate({ path: 'friends', select: '-__v' })
+        .select('-__v')
         .then((dbUserData) => res.json(dbUserData))
         .catch((err) => {
             console.log(err);
@@ -18,10 +21,9 @@ const userController = {
 
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-        .populate({
-            path: 'thoughts',
-            select: '-__v',
-        })
+        .populate({ path: 'thoughts', select: '-__v' })
+        .populate({ path: 'friends', select: '-__v' })
+        .select('-__v')
         .then((dbUserData) => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'No user found with this id!' });
@@ -62,5 +64,7 @@ const userController = {
         .catch((err) => res.status(400).json(err));
     }
 };
+
+
 
 module.exports = userController;
