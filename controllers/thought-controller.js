4d9ba9a8115f519895df9919d4e2824d7efb635c
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-01-23 22:00:41 -0600

    setup thought routes and logicl

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,42 @@
-// code 
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    getThoughtById({ params }, res) {
+
+    },
+
+    createThought({ params, body }, res) {
+
+    },
+
+    updateThought({ params, body }, res) {
+
+    },
+
+    deleteThought({ params }, res) {
+
+    },
+
+    addReaction({ params, body }, res) {
+
+    },
+
+    deleteReaction({ params }, res) {
+
+    }
+
+
+}
+
+module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,22 @@
-// code 
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts);
+
+router.route('/:userId').post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
